fd7b1d06312f3645fbff47b57e2e3ce9b062c9aa
Kal Krueger <kkrueger19@gmail.com>
2020-09-27 09:38:26 -0600

    methods commit 1

---
+++ methods/greeting_ex1_ex2.rb
@@ -0,0 +1,13 @@
+def greeting(name)
+  puts "Nice to see you, #{name}."
+end
+
+greeting("Willow")
+
+
+#ex2
+  #1. return = 2
+  #2. return = nil
+  #3. return = "Joe"
+  #4. return = "four"
+  #5. return = nil
---
+++ methods/scream_ex4_ex5_ex6.rb
@@ -0,0 +1,8 @@
+def scream(words)
+  puts words + "!!!!"
+end
+
+scream("Yippeee")
+
+
+#The method "calculate_product" requires 2 arguments but only one has been provided
